447cec1ba1c3b818002f8ae50ab924e5a0f66115
krayf <freestonekray@gmail.com>
2022-02-07 18:21:56 -0500

    moving more functions to IDW object

--- process.py
+++ process.py
@@ -1,25 +1,40 @@
-import geopandas as gpd
 from os.path import join
-import idw
-import matplotlib.pyplot as plt
 
-def plot(x,y,z,grid):
-    plt.figure()
-    plt.imshow(grid, extent=(x.min(), x.max(), y.max(), y.min()))
-    # plt.hold(True)
-    # plt.scatter(x,y,c=z)
-    plt.colorbar()
+import geopandas as gpd
+import numpy as np
 
-print('read file...')
+from idw import IDWGenerator
+
+#...............................................................................
+NNEAR = 8  # number of nearest neighbors to look for
+EPS = .1  # approximate nearest, dist <= (1 + eps) * true nearest
+P = 2  # weights ~ 1 / distance**p
+RESOLUTION = 500
+
 gdf = gpd.read_file(join('shapefiles', 'well_nitrate.shp'))
-x = gdf.geometry.x.to_numpy()
-y = gdf.geometry.y.to_numpy()
+gdf['x'] = gdf.geometry.x
+gdf['y'] = gdf.geometry.y
+xy = gdf[['x', 'y']].to_numpy()
 z = gdf.nitr_ran.to_numpy()
+# ask = generate_query_points(gdf, RESOLUTION)
 
-print('Start idw.main')
-idw_results = idw.main(x, y, z, p=1.5, n=50)
-grid1 = idw_results['z_idw']
-print('done!')
 
-# for plotting
-plot(x, y, z, grid1)
+idw_gen = IDWGenerator(xy, z, gdf.total_bounds)
+ask = idw_gen.generate_query_points(resolution=RESOLUTION)
+interpol = idw_gen.interpolate(ask, nnear=NNEAR, eps=EPS, p=P )
+
+## get it into a grid, flip it (for some reason it is upside-down)
+final_grid = interpol.reshape((RESOLUTION, RESOLUTION))
+# final_grid = np.transpose(final_grid)
+final_grid = np.flip(final_grid, 0)
+
+# export to tif
+raster_file = 'myraster_flip.tif'
+idw_gen.export_to_tif(final_grid, raster_file)
+
+# calc zonal stats by reading in tif
+# possible it could just read in an array instead of first exporting to 
+# tif first
+vector_file = join('shapefiles', 'cancer_tracts_wgs84.shp')
+output_file = join('shapefiles', 'aggregateNitrate.shp')
+idw_gen.calculate_zonal_stats(vector_file, raster_file, output_file)
